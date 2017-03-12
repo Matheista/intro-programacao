599158907be9fe47441c12b40876e3c118b6a06a
Matheista <marlon.ferreira@dce.ufpb.br>
2017-03-12 10:49:07 -0300

    Add files via upload

--- Roteiro(questao 1-5)-Aula-Eduardo .py
+++ Roteiro(questao 1-5)-Aula-Eduardo .py
@@ -1,4 +1,60 @@
-i = 3
+#1
+"""n1 = int(input("primeiro número: ")) 
+
+n2 = int(input("primeiro número: "))
+
+soma=n1+n2
+
+print(soma)"""
+
+
+
+#2
+"""n1 = int(input("primeiro número: ")) 
+
+n2 = int(input("primeiro número: "))
+
+produto=n1*n2
+soma=n1+n2
+
+
+
+print(soma,produto)"""
+
+
+#3
+  """  n1 = int(input("primeiro número: "))
+    n2 = int(input("primeiro número: "))
+
+    if n1 > 0 and n2 > 0:
+        soma=n1+n2
+        print(soma)
+        produto=n1*n2
+        print(produto)
+    else:
+        print("Você digitou um número inválido")"""
+
+    
+
+#4
+  """n1 = int(input("primeiro número: "))
+    n2 = int(input("primeiro número: "))
+
+    if n1 > 0 and n2 > 0:
+        soma=n1+n2
+        print(soma)
+        produto=n1*n2
+        print(produto)
+    else:
+        media=(n1+n2/2)
+        print(media)"""
+
+        
+
+#5
+
+
+"""i = 3
 while i > 0:
     n1 = int(input("primeiro número: "))
     n2 = int(input("primeiro número: "))
@@ -12,4 +68,4 @@ while i > 0:
         media=(n1+n2/2)
         print(media)
 
-    i-= 1
+    i-= 1"""
